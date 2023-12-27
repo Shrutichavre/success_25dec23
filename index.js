b03f7bee708395d982681a0dc1dc302d3603d075
Shrutichavre <153539679+Shrutichavre@users.noreply.github.com>
2023-12-27 21:00:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const con = mysql.createConnection({
+	host:"sql12.freesqldatabase.com",
+	user:"sql12673077",
+	password:"cBXMuafBva",
+	database:"sql12673077"
+});
+
+app.post("/save",(req,res) =>{
+	let data = [req.body.name,req.body.company,req.body.pkg];
+	console.log(data);
+	let sql ="insert into student1 values(?,?,?)";
+	con.query(sql,data,(err,result) =>{
+		if(err) res.send(err);
+		else		res.send(result);
+	});
+});
+
+app.listen(9000,() =>{console.log("ready to serve @9000");})
